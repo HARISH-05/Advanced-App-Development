e3ccf8d36fa5daff447c1a761366352b130b6ee4
HARISH-05 <113657265+HARISH-05@users.noreply.github.com>
2024-03-05 10:42:09 +0530

    Add files via upload

---
+++ Backend/Day3/src/main/java/com/example/backend/entity/userBooking.java
@@ -0,0 +1,35 @@
+package com.example.backend.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="user_booking")
+public class userBooking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+        private String fullName;
+        private String email;
+        private Long phone;
+        private Date startDate;
+        private Date endDate;
+        private int numGuests;
+        private String address;
+        private String state;
+        private String country;
+
+}
