b5603b2791f8ebe48c06e8a20f7506a21d37bc05
HARISH-05 <113657265+HARISH-05@users.noreply.github.com>
2024-03-07 12:26:13 +0530

    Add files via upload

---
+++ Frontend/Day6/src/components/card.jsx
@@ -0,0 +1,39 @@
+import  { useState } from 'react';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+import '../assets/card.css'
+
+const FlipCard = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleSignUpClick = () => {
+    setIsFlipped(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsFlipped(false);
+  };
+
+  return (
+    <div className='flip'>
+      <div>
+        <h1>My Headings</h1>
+      </div>
+      <div>
+    <div className={`flip-card ${isFlipped ? 'flipped' : ''}`}>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <LoginForm onSignUpClick={handleSignUpClick} />
+        </div>
+        <div className="flip-card-back">
+          <RegisterForm onSignInClick={handleSignInClick} />
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+  );
+};
+
+export default FlipCard;
+
